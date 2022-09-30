95c9c235f46d26cc6c8928ede766002bf9428177
Amit Shukla <56184462+Amitshu2003@users.noreply.github.com>
2022-10-01 00:07:50 +0530

    delete recursive ll

---
+++ Delete node (recursive) LL.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node *next;
+    Node(int d){
+        data = d;
+        next = NULL;
+    }
+};
+
+Node *takeinput(){
+    int data;
+    cin>>data;
+    Node *head = NULL;
+    Node *tail = NULL;
+    while(data != -1){
+        Node *newnode = new Node(data);
+        if(head ==NULL){
+            head = newnode;
+            tail = newnode;
+        }
+        else{
+            tail->next = newnode;
+            tail=tail->next;            
+        }
+        cin>>data;
+    }
+    return head;
+}
+
+void print(Node *head){
+    Node*temp = head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+    cout<<endl;
+}
+
+Node *deleteAtRecursively(Node *head, int i){
+    if(head == NULL)
+    return head;
+    if(i==0){
+        Node * a = head;
+        head = head->next;
+        delete a;
+        return head;
+    }
+    Node *x = deleteAtRecursively(head->next,i-1);
+    head->next = x;
+    return head;
+}
+
+int main(){
+    Node *head = takeinput();
+    int i;
+    cin>>i;
+    Node *h = deleteAtRecursively(head, i);
+    print(h);
+}
