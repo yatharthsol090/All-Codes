7e62af9be2e4f3c0eb80b76e432226becf2df8f1
yatharthsol090 <56156585+yatharthsol090@users.noreply.github.com>
2022-10-03 13:44:08 +0530

    new_files

---
+++ coin.cpp
@@ -0,0 +1,26 @@
+	#include<bits/stdc++.h>
+	using namespace std;
+
+	int MinimumCoins(int coins[], int m, int V)
+	{
+	if (V == 0) return 0;
+	int res = INT_MAX;
+	for (int i=0; i<m; i++)
+	{
+		if (coins[i] <= V)
+		{
+			int sub_res = MinimumCoins(coins, m, V-coins[i]);
+			if (sub_res != INT_MAX && sub_res + 1 < res)
+			res = sub_res + 1;
+		}
+	}
+	return res;
+	}
+	int main()
+	{
+		int coins[] = {9, 6, 5, 1};
+		int m = sizeof(coins)/sizeof(coins[0]);
+		int V = 11;
+		cout << "Minimum coins required is: "<< MinimumCoins(coins, m, V);
+		return 0;
+	}
